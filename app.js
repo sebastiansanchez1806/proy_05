c78050a21f1b2f1f8e562339dbef24f762e25b9c
Johan024 <jevergara987@misena.edu.co>
2023-08-16 15:07:22 -0500

    agregue puntos y comas

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require('colors');
 
 const main = async() => {
-    console.clear()
-    console.log('quiubo puesss'.red)
+    console.clear();
+    console.log('quiubo puesss'.red);
 
     console.log('buenas tardes'.blue);
     console.log('buenas buenas');
